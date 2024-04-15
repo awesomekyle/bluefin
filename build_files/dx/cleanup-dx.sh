74827830a4e0bf802f28b69a68f348dbd3bfc03f
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-15 14:55:52 -0400

    remove lxd and charm

--- build_files/dx/cleanup-dx.sh
+++ build_files/dx/cleanup-dx.sh
@@ -7,5 +7,4 @@ rm -f /etc/yum.repos.d/ganto-lxc4-fedora-"${FEDORA_MAJOR_VERSION}".repo
 rm -f /etc/yum.repos.d/karmab-kcli-fedora-"${FEDORA_MAJOR_VERSION}".repo
 rm -f /etc/yum.repos.d/atim-ubuntu-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo
 rm -f /etc/yum.repos.d/vscode.repo
-rm -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo
 rm -f /etc/yum.repos.d/fedora-cisco-openh264.repo
