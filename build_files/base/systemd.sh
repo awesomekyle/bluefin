f570574cc2285ccb2096a8dbdcf13767e08ef6bb
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-19 02:35:25 +0000

    add tailscale

--- build_files/base/systemd.sh
+++ build_files/base/systemd.sh
@@ -3,6 +3,7 @@
 set -ouex pipefail
 
 systemctl enable rpm-ostree-countme.service
+systemctl enable tailscaled.service
 systemctl enable dconf-update.service
 systemctl --global enable ublue-flatpak-manager.service
 systemctl enable ublue-update.timer
