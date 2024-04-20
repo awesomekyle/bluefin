7462522b0ce5eb0b571f265c36862cc15cae063d
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-20 02:45:20 +0000

    add Iosevka font

--- build_files/base/font-install.sh
+++ build_files/base/font-install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
 set -ouex pipefail
-fc-cache -f /usr/share/fonts/ubuntu 
+fc-cache -f /usr/share/fonts/ubuntu
 fc-cache -f /usr/share/fonts/inter
+
+curl -s 'https://api.github.com/repos/be5invis/Iosevka/releases/latest' | \
+    jq -r ".assets[] | \
+    .browser_download_url" | \
+    grep "PkgTTC-Iosevka-" | \
+    xargs -n 1 curl -L -o /tmp/PkgTTC-Iosevka.zip --fail --silent --show-error
+unzip /tmp/PkgTTC-Iosevka.zip -d /usr/share/fonts/iosevka
