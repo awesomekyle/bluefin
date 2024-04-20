c566f8b20c5dc28ad7eac697c80d387bb5fad501
Kyle Weicht <303261+awesomekyle@users.noreply.github.com>
2024-04-20 23:56:35 +0000

    switch to bluefin dx cli

--- system_files/shared/usr/libexec/enable-bluefin-cli.sh
+++ system_files/shared/usr/libexec/enable-bluefin-cli.sh
@@ -10,12 +10,12 @@ source /usr/lib/ujust/ujust.sh
 bluefin_cli=(${red}Disabled${n} ${red}Inactive${n} ${red}Not Default${n})
 
 function get_status(){
-    if systemctl --quiet --user is-enabled bluefin-cli.target; then
+    if systemctl --quiet --user is-enabled bluefin-dx-cli.target; then
         bluefin_cli[0]="${green}Enabled${n}"
     else
         bluefin_cli[0]="${red}Disabled${n}"
     fi
-    if systemctl --quiet --user is-active bluefin-cli.service; then
+    if systemctl --quiet --user is-active bluefin-dx-cli.service; then
         bluefin_cli[1]="${green}Active${n}"
     else
         bluefin_cli[1]="${red}Inactive${n}"
@@ -46,22 +46,22 @@ function default_login(){
 function logic(){
     if test "$toggle" = "Enable"; then
         echo "${b}${green}Enabling${n} Bluefin-CLI"
-        systemctl --user enable --now bluefin-cli.target > /dev/null 2>&1 
-        if ! systemctl --quiet --user is-active bluefin-cli.service; then
-            systemctl --user reset-failed bluefin-cli.service > /dev/null 2>&1 || true
+        systemctl --user enable --now bluefin-dx-cli.target > /dev/null 2>&1
+        if ! systemctl --quiet --user is-active bluefin-dx-cli.service; then
+            systemctl --user reset-failed bluefin-dx-cli.service > /dev/null 2>&1 || true
             echo "${b}${green}Starting${n} Bluefin-CLI"
-            systemctl --user start bluefin-cli.service
+            systemctl --user start bluefin-dx-cli.service
         fi
         default_login
     elif test "$toggle" = "Disable"; then
         echo "${b}${red}Disabling${n} Bluefin-CLI"
-        systemctl --user disable --now bluefin-cli.target > /dev/null 2>&1
-        if systemctl --quiet --user is-active bluefin-cli.service; then
+        systemctl --user disable --now bluefin-dx-cli.target > /dev/null 2>&1
+        if systemctl --quiet --user is-active bluefin-dx-cli.service; then
             echo "Do you want to ${b}${red}Stop${n} the Container?"
             stop=$(Confirm)
             if test "$stop" -eq 0; then
-                systemctl --user stop bluefin-cli.service > /dev/null 2>&1
-                systemctl --user reset-failed bluefin-cli.service > /dev/null 2>&1 || true
+                systemctl --user stop bluefin-dx-cli.service > /dev/null 2>&1
+                systemctl --user reset-failed bluefin-dx-cli.service > /dev/null 2>&1 || true
             fi
         fi
         echo "Setting Host back to default Ptyxis Profile"
